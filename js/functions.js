00f7c6edc240be319875e96846f97f1759cf997a
sarhov <sarhov@sarhov.com>
2014-10-26 15:01:29 +0400

    add version for 825 px

--- js/functions.js
+++ js/functions.js
@@ -2,18 +2,37 @@ $(document).ready(function() {
   //check if used option is checked
   function checkUsed(){
     if ($('#switcher2').is(':checked')) {
-      $('.jsUsedHidden').hide();
-      $('.filter-box_used').show()
-    }
-    else{
-      $('.jsUsedHidden').show();
-       $('.filter-box_used').hide()
-
+      if ($(window).width() > 825) {
+       $('.jsUsedHidden').hide();
+     }
+     
+     $('.filter-box_used').show()
+     if ($(window).width() <= 825) {
+      $('.jsUsedMiddle').hide();  
     }
   }
+  else{
+    if ($(window).width() > 825) {
+     $('.jsUsedHidden').show();
+   }
+   $('.filter-box_used').hide()
+   $('.jsUsedMiddle').show();  
+
+ }
+ if ($(window).width() <= 825) {
+  var block =  $('.jsUsedHidden').detach();
+  $('.filters-block_advanced > div:last-child').after(block)
+}
+}
 
   //run function to check if used fuction is checked
-  checkUsed()
+  checkUsed();
+  $(window).on('resize', function(){
+   checkUsed();
+      // var win = $(this); //this = window
+      // if (win.width() <= 825) { /* ... */ }
+      // if (win.width() >= 1280) { /* ... */ }
+    });
 
   //run the function above, on click
   $('.filters-switchers__item, .filter-reset').click(function() {
@@ -34,27 +53,72 @@ $(document).ready(function() {
     var parent = $(this).parents('.filters-block');
     console.log(parent)
     parent.find('.filter-box').not('.filter-box_actions').hide();
-    }, function() { 
-var parent = $(this).parents('.filters-block');
-     parent.find('.filter-box').not('.filter-box_actions').show();
+  }, function() { 
+    var parent = $(this).parents('.filters-block');
+    parent.find('.filter-box').not('.filter-box_actions').show();
 
     //in this part we must be sure that the block  which was hidden because of used option, will not be visible after mouse out
-    if ($('#switcher2').is(':checked')) {
+    if ($('#switcher2').is(':checked')) { 
+      if ($(window).width() <= 825) {
+        $('.jsUsedMiddle').hide();  
+      }
+ if ($(window).width() > 825) {
      $('.jsUsedHidden').hide();
    }
-   else{
+    
+    }
+    else{
      $('.filter-box_used').hide()
    }
  });
 
 
-$('.jsAdvancedCall').click(function(event) {
-  event.preventDefault();
-  $('.filters-block_advanced').toggle();
+  $('.jsAdvancedCall').click(function(event) {
+    event.preventDefault();
+    $('.filters-block_advanced').toggle();
   // $('.filters-switchers__item').toggleClass('is-advanced');
 });
 
 
+  var isMobile = {
+    Android: function() {
+      return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+      return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+      return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+      return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+      return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+      return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+  };
+  if(isMobile.any()) {
+    $('body').addClass('mobile');
+  }
+  else{
+    $('body').addClass('desktop')
+  }
+// $('.mobile .header-menu a').on('click', function (e) {
+// e.preventDefault();
+// if ($(this).parents('li:first').hasClass('hassub')) {
+// var $this = $(this).parents('li:first');
+// if ($this.hasClass('touch-active')) {
+// location.href = $(this).attr('href');
+// } else {
+// $('.touch .header-menu li').removeClass('touch-active');
+// $this.addClass('touch-active');
+// }
+// } else {
+// location.href = $(this).attr('href');
+// }
 
 
   // ready
@@ -88,4 +152,3 @@ function customSelect (theselect, thevalue) {
     SELECT.find(' option[value="'+ needOption +'"]').attr('selected', 'selected').siblings().attr('selected', false).parent('select').change();
   });
 }
-  
